42fd3fd542ff35e0683db927f3e23f9b6962121d
Keith Fiske <keith@keithf4.com>
2019-06-25 12:53:51 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+## Setup
+If installing from source, run make from with source directory
+```
+make install
+```
+
+Create an fdw server & user mapping manually first with your preferred credentials. Leave server name as "pgbouncer". pgbouncer statistics are global so it only needs to be monitored from a single database. If you have multiple databases in your cluster, it is recommended to just install it to the default `postgres` database.
+```
+CREATE SERVER pgbouncer FOREIGN DATA WRAPPER dblink_fdw OPTIONS (host 'localhost',
+                                                                 port '5432',
+                                                                 dbname 'pgbouncer');
+
+CREATE USER MAPPING FOR PUBLIC SERVER pgbouncer OPTIONS (user 'ccp_monitoring', password 'mypassword');
+```
+The dblink extension must be created in a schema that is within the search path of the role that was used for the above user mapping. A default location of the PUBLIC schema is the easiest.
+```
+CREATE EXTENSION dblink;
+```
+
+Recommend placing this extension's objects in their own dedicated schema
+```
+CREATE SCHEMA pgbouncer;
+
+CREATE EXTENSION pgbouncer_fdw SCHEMA pgbouncer;
+```
+
+Grant necessary permissions on extension objects to the user mapping role
+```
+GRANT USAGE ON FOREIGN SERVER pgbouncer TO ccp_monitoring;
+
+GRANT USAGE ON SCHEMA pgbouncer TO ccp_monitoring;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA pgbouncer TO ccp_monitoring;
+```
---
+++ sql/pgbouncer_fdw.sql
@@ -0,0 +1,141 @@
+CREATE VIEW @extschema@.clients AS
+    SELECT * FROM dblink('pgbouncer', 'show clients') AS x
+    (   type text
+        , "user" text
+        , database text
+        , state text
+        , addr text
+        , port int
+        , local_addr text
+        , local_port int
+        , connect_time timestamp with time zone
+        , request_time timestamp with time zone
+        , wait int
+        , wait_us int
+        , close_needed int
+        , ptr text
+        , link text
+        , remote_pid int
+        , tls text);
+
+CREATE VIEW @extschema@.config AS
+    SELECT * FROM dblink('pgbouncer', 'show config') AS x 
+    (   key text
+        , value text
+        , changeable boolean);
+
+CREATE VIEW @extschema@.databases AS
+    SELECT * FROM dblink('pgbouncer', 'show databases') AS x
+    (   name text
+        , host text
+        , port int
+        , database text
+        , force_user text
+        , pool_size int
+        , reserve_pool int
+        , pool_mode text
+        , max_connections int
+        , current_connections int
+        , paused int
+        , disabled int);
+
+CREATE VIEW @extschema@.dns_hosts AS
+    SELECT * FROM dblink('pgbouncer', 'show dns_hosts') AS x
+    (   hostname text
+        , ttl bigint
+        , addrs text);
+
+CREATE VIEW @extschema@.dns_zones AS
+    SELECT * FROM dblink('pgbouncer', 'show dns_zones') AS x
+    (   zonename text
+        , serial text
+        , count int);
+CREATE VIEW @extschema@.lists AS
+    SELECT * FROM dblink('pgbouncer', 'show lists') AS x 
+    (   list text
+        , items int);
+
+CREATE VIEW @extschema@.pools AS
+    SELECT * FROM dblink('pgbouncer', 'show pools') AS x
+    (   database text
+        , "user" text
+        , cl_active int
+        , cl_waiting int
+        , sv_active int
+        , sv_idle int
+        , sv_used int
+        , sv_tested int
+        , sv_login int
+        , maxwait int
+        , maxwait_us int
+        , pool_mode text);
+
+CREATE VIEW @extschema@.servers AS
+    SELECT * FROM dblink('pgbouncer', 'show servers') AS x 
+    (   type text
+        , "user" text
+        , database text
+        , state text
+        , addr text
+        , port int
+        , local_addr text
+        , local_port int
+        , connect_time timestamp with time zone
+        , request_time timestamp with time zone
+        , wait int
+        , wait_us int
+        , close_needed int
+        , ptr text
+        , link text
+        , remote_pid int
+        , tls text);
+
+CREATE VIEW @extschema@.sockets AS
+    SELECT * FROM dblink('pgbouncer', 'show sockets') AS x
+    (   type text
+        , "user" text
+        , database text
+        , state text
+        , addr text
+        , port int
+        , local_addr text
+        , local_port int
+        , connect_time timestamp with time zone
+        , request_time timestamp with time zone
+        , wait int
+        , wait_us int
+        , close_needed int
+        , ptr text
+        , link text
+        , remote_pid int
+        , tls text
+        , recv_pos int
+        , pkt_pos int
+        , pkt_remain int
+        , send_pos int
+        , send_remain int
+        , pkt_avail int
+        , send_avail int);
+
+CREATE VIEW @extschema@.stats AS
+    SELECT * FROM dblink('pgbouncer', 'show stats') AS x
+    (   database text
+        , total_xact_count bigint
+        , total_query_count bigint
+        , total_received bigint
+        , total_sent bigint
+        , total_xact_time bigint
+        , total_query_time bigint
+        , total_wait_time bigint
+        , avg_xact_count bigint
+        , avg_query_count bigint
+        , avg_recv bigint
+        , avg_sent bigint
+        , avg_xact_time bigint
+        , avg_query_time bigint
+        , avg_wait_time bigint );
+
+CREATE VIEW @extschema@.users AS
+    SELECT * FROM dblink('pgbouncer', 'show users') AS x
+    (   name text
+        , pool_mode text);
